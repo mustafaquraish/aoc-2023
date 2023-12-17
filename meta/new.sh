568af04ca480439e5845943ca54db8cda5675ffe
Mustafa Quraish <mustafaq9@gmail.com>
2023-12-17 04:35:12 -0500

    fix new script

--- meta/new.sh
+++ meta/new.sh
@@ -6,6 +6,8 @@ DAY=$(printf "%02d" $1)
 cp src/template.oc src/$DAY.oc
 code-insiders src/$DAY.oc
 
-until ./meta/fetch.py 2023 $DAY --sample > input/$DAYs.txt; do sleep 1; done
-until ./meta/fetch.py 2023 $DAY > input/$DAY.txt; do sleep 1; done
+until ./meta/fetch.py 2023 $DAY --sample -o input/${DAY}s.txt; do sleep 1; done
+until ./meta/fetch.py 2023 $DAY -o input/$DAY.txt; do sleep 1; done
 
+DAY_NO_PREFIX=$(printf "%d" $DAY)
+open https://adventofcode.com/2023/day/$DAY_NO_PREFIX
