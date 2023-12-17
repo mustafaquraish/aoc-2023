4d7db0441e2bb9669d599abca5e2ce664a4ac9b0
Mustafa Quraish <mustafaq9@gmail.com>
2023-12-17 15:55:30 -0500

    better profiling in bench.py

--- meta/bench.py
+++ meta/bench.py
@@ -8,6 +8,7 @@ import subprocess
 import shutil
 import textwrap
 from glob import glob
+import json
 from time import perf_counter
 
 YEAR = 2023
@@ -32,14 +33,14 @@ def compile_benchmark():
         print("[+] Compiling with instrumentation")
         cmd("ocen src/run_all.oc -o ./run_all -cf '-O3 -march=native -funroll-loops -Wno-unused-result -fprofile-generate'")
         print("[+] Running benchmark")
-        cmd("./run_all ./")
+        cmd("./run_all ./ 2")
         print("[+] Compiling with profile")
         cmd("ocen src/run_all.oc -o ./run_all -cf '-O3 -march=native -funroll-loops -Wno-unused-result -fprofile-use'")
     elif sys.platform == "darwin":
         print("[+] Compiling with instrumentation")
         cmd("ocen src/run_all.oc -o ./run_all -cf '-O3 -march=native -funroll-loops -Wno-unused-result -fprofile-generate'")
         print("[+] Running benchmark")
-        cmd("./run_all ./")
+        cmd("./run_all ./ 2")
         # use llvm-profdata to merge the profiles
         print("[+] Merging profiles with llvm-profdata")
         cmd("xcrun llvm-profdata merge -output=default.profdata *.profraw")
@@ -66,42 +67,79 @@ def create_benchmark(days):
         fp.write("\n")
 
         prelude = textwrap.dedent("""
-        def call_fn(f: fn(i32,&str), inp: str) {
+        import std::vector::{ Vector }
+        import std::buffer::{ Buffer }
+                              
+        @compiler c_include "time.h"
+        def clock(): i64 extern
+        const CLOCKS_PER_SEC: i64 extern
+
+        def ftime(): f32 => clock() as f32 / CLOCKS_PER_SEC as f32
+        let RUNS: u32 = 20
+                                  
+        def call_fn(times: &Vector<f32>, f: fn(i32,&str), inp: str) {
             let argv: [str; 2]
             argv[0] = "dummy"
             argv[1] = inp
-            f(2, argv)
+            let total = 0.0
+            for let i = 0; i < RUNS; i++ {                  
+                let start = ftime()
+                f(2, argv)
+                let end = ftime()
+                total += end - start
+            }
+            let avg = total / RUNS as f32
+            times.push(avg)
         }
 
         def main(argc: i32, argv: &str) {
             assert argc > 1, "no input directory provided"
+            
+            if argc > 2 {
+                RUNS = argv[2].to_u32()
+            }
+                                  
+            let times = Vector<f32>::new()
             let dir = argv[1]
         """)
         fp.write(prelude)
 
         for day in days:
-            fp.write(f"    call_fn(day{day:02}::main, `{{dir}}/{day:02}.in`)\n")
+            fp.write(f"    call_fn(times, day{day:02}::main, `{{dir}}/{day:02}.in`)\n")
 
-        fp.write("}\n")
+        fp.write(textwrap.dedent("""
+
+            let buf = Buffer::make()
+            buf.puts("{\\n")
+            let total = 0.0
+            for let i = 0; i < times.size; i++ {
+                let t = times.at(i)
+                total += t
+                buf.putsf(`    "{i+1:02d}": {t:.3f},\\n`)                        
+            }
+            buf.putsf(`    "total": {total:.3f}\\n`)
+            buf.puts("}\\n")
+
+            let file = std::File::open("benchmark.json", "w")
+            file.write(buf.data, buf.size)
+            file.close()
+        }                         
+        """))
 
     with pushd("build"):
         compile_benchmark()
 
 
 def run_benchmark():
-    times = []
-    RUNS = 20
-    for i in range(RUNS):
-        start = perf_counter()
-        cmd("./build/run_all ./build")
-        end = perf_counter()
-        if i > 2:
-            times.append(end - start)
-        print(f"[+] Running benchmark: {i+1:>3d} /{RUNS:>3d}\r", end="", flush=True)
-    avg = sum(times) / len(times)
-    print()
+    with pushd("build"):
+        print("[+] Running benchmark")
+        cmd("./run_all ./")
+        assert os.path.exists("benchmark.json"), "benchmark.json not found"
+        print("[+] Benchmark results:")
+        res = json.load(open("benchmark.json"))
+        for k, v in res.items():
+            print(f" {k:>6s}: {v:.3f}s")
 
-    print(f"[+] Average time for all {len(days)} days: {avg:.3f}s")
 
 files = sorted(glob("src/??.oc"))
 days = [int(re.search(r"(\d+)", f).group(1)) for f in files]
